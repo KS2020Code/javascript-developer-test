315ae1a9344b724f36ac1005cce4fb2d38f864bd
KS2020Code <singhkuljeet101@gmail.com>
2022-03-02 22:53:37 +1100

    Solution provided

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,34 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+	let results = [];
+	//Null check for urls
+	if (urls.length > 0) {
+		//Create an array for promises for number of urls
+		const pmsAry = [];
+		for (let i = 0; i < urls.length; i++) {
+			pmsAry.push(httpGet(urls[i]));
+		}
+
+		//Use Promise.allSettled() to get all the responses (success and failure) and pass array of promises to it
+		await Promise.allSettled(pmsAry).then((responses) => {
+			//Iterate through the responses to add each response message to results array
+			for (let i = 0; i < responses.length; i++) {
+				const data = responses[i].value;
+				const responseData = JSON.parse(data.body);
+				//Success condition
+				if (data.status == 200) {
+					results.push({ "Arnie Quote": responseData.message });
+				} else {
+					//Failure Condition
+					results.push({ FAILURE: responseData.message });
+				}
+			}
+		});
+	}
+	return results;
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
